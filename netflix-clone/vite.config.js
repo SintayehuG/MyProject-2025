0d7d3471c5009d80dac1d206d269b3840962e69e
SintayehuG <sintayehu.getahun@gmail.com>
2025-10-30 10:38:55 -0500

    vite updated

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/MyProject-2025"
+  // base:"/MyProject-2025"
+  base:"./"
 })
